f1bca5223084697827b9a9894805346c9db7d1e8
Sergiu <nixigg@gmail.com>
2019-07-24 00:24:46 -0700

    Initial commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.json({msg: 'Welcome to the Contact Manager'})
 })
 
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
