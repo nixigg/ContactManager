26d1607b40ec8663c0a48d5b41c6616915b42e4d
Sergiu <nixigg@gmail.com>
2019-07-24 00:30:37 -0700

    express, basic routes

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
