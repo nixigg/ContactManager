2888437cd960d88aac4d31583f65093a2307398e
Sergiu <nixigg@gmail.com>
2019-08-08 00:02:11 -0700

    Added CRUD functionality

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,33 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import ContactContext from '../../context/contact/contactContext';
 
-const Navbar = (props) => {
+const Navbar = props => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
+
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  }
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href="#!" onClick={onLogout}>
+          <i className="fas fa-sign-out-alt" />{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
         <i className={props.icon} /> {props.title}
       </h1>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navbar.defaultProps = {
-  title: 'Contact Manager',
-  icon: 'fas fa-id-card-alt'
-}
+  title: "Contact Manager",
+  icon: "fas fa-id-card-alt"
+};
 
-export default Navbar
+export default Navbar;
